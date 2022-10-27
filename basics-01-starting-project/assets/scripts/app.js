e3b27f3784d2f2098ca0c7ffe53ef1c7d584d583
Alex <opetrovskyi423@gmail.com>
2022-10-27 22:32:46 +0200

    35

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,17 @@
-console.log('test')
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+
+function add() {
+    currentResult='';
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, calculationDescription);
+}
+
+addBtn.addEventListener('click', add)
+
+
+let calculationDescription = 'xXe\\!s'
+
+
+
